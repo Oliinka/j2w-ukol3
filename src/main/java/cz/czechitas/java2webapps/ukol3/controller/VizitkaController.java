491f389e561b9b808a4b1649aa7ab456ef9527ef
Olinka <olga.titzenthaler@gmail.com>
2024-05-07 00:14:27 +0200

    Java bean Vizitka @Controller public class VizitkaController
    chybí ošetřit null hodnoty

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -1,20 +1,37 @@
 package cz.czechitas.java2webapps.ukol3.controller;
 
+import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Kontroler obsluhující zobrazování vizitek.
  */
 @Controller
 public class VizitkaController {
-    @GetMapping("/")
-    public String seznam() {
-        return "seznam";
-    }
+        private final List<Vizitka> seznamVizitek = List.of(
+                new Vizitka("Dita Formánková", "Czechitas.cz", "Václavské nám. 837/11", "11000 Praha 1", "dita@czechitas.cz", "+420 800 123 456", "www.czechitas.cz"),
+                new Vizitka("Monika Ptáčníková", "Czechitas.cz", "Václavské nám. 837/11", "11000 Praha 1", "monika@czechitas.cz", "+420 800 123 456", "www.czechitas.cz"),
+                new Vizitka("Jan Nováková", "Czechitas z. s.", "Václavské náměstí 837/11", "11000 Praha 1", "", "+420 800 123 456", "www.czechitas.cz"),
+                new Vizitka("Mirka Zatloukalová", "Czechitas z. s.", "Václavské náměstí 837/11", "11000 Praha 1", "mirka@czechitas.cz", "", "www.czechitas.cz")
+        );
 
-    @GetMapping("/detail")
-    public String detail() {
-        return "detail";
+        @GetMapping("/")
+        public ModelAndView seznam() {
+            ModelAndView modelAndView = new ModelAndView("seznam");
+            modelAndView.addObject("seznamVizitek", seznamVizitek);
+            return modelAndView;
+        }
+
+        @GetMapping("/{index}") //index do slozenych zavorek
+
+        public ModelAndView detail(@PathVariable int index) {
+            ModelAndView modelAndView = new ModelAndView("detail");
+            modelAndView.addObject("vizitka", seznamVizitek.get(index));
+            return modelAndView;
+        }
     }
-}
